caa71dc341d03af1634185e61c4ee79d29fbf839
Laura_Long <laura_long@Laura-LongdeMBP.lan>
2020-05-30 22:41:22 +0800

    看了下图，修订了gitignore

--- dashapp/image.py
+++ dashapp/image.py
@@ -10,6 +10,7 @@ img_path_list = []
 for root, dirs_, files_ in os.walk('./static/img'):
     for file_ in files_:
         img_path_list.append(os.path.join(root, file_))
+img_path_list.sort()
 
 
 # img_path_list = [os.path.join('/static/img', i) for i in os.listdir('./static/img')]
@@ -17,7 +18,8 @@ print(img_path_list)
 
 app.layout = html.Div([
     html.Div(
-        [html.A(html.Img(src=img_path, style={'max-width': '25%', 'max-height': '340px', 'float': 'left'}), href=img_path, target='_self') for img_path in img_path_list]
+        [html.A(html.Img(src=img_path, style={'max-width': '25%', 'max-height': '200px', 'float': 'left'}), href=img_path, target='_blank')
+         for img_path in img_path_list]
     )
 ])
 
--- dashapp/test.sh
+++
@@ -1,8 +0,0 @@
-irm() {
-    ipath=/data1/trash/$(date +%Y%m%d)
-
-    if [ ! -d $ipath ]; then
-        mkdir $ipath
-    fi
-    mv $1 $ipath && echo "$1 已放入回收站"
-}
