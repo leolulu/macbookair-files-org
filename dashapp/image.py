7cb6a053759d2404e92be442d1c859f753ebf679
leolulu <348699103@qq.com>
2020-07-31 16:57:37 +0800

    新增查看剩余图片张数功能,新增通过滑条更改每页图片数功能

--- dashapp/image.py
+++ dashapp/image.py
@@ -6,6 +6,7 @@ import os
 
 app = dash.Dash(__name__)
 
+page_capacity = 50
 img_path_list = []
 for root, dirs_, files_ in os.walk('./static/img'):
     for file_ in files_:
@@ -24,19 +25,26 @@ img_path_list.sort()
 
 app.layout = html.Div([
     html.Div(style={'display': 'flex', 'flex-wrap': 'wrap', 'justify-content': 'left'}, id='container'),
-    html.Div(html.A(html.Button('再来100张！', id='get_pics'), href='#container'), id='button_container')
+    html.Div([
+        html.Div(html.A(html.Button([html.Div(id='button_text'), html.Div(id='remain_count')], id='get_pics'), href='#container')),
+        html.Div(dcc.Slider(min=10, max=500, step=10, value=page_capacity, updatemode='drag', id='slider1'))
+    ], id='button_container'
+    )
 ])
 
 
 @app.callback(
-    dash.dependencies.Output('container', 'children'),
+    [
+        dash.dependencies.Output('container', 'children'),
+        dash.dependencies.Output('remain_count', 'children')
+    ],
     [
         dash.dependencies.Input('get_pics', 'n_clicks')
     ]
 )
 def popup_100_pics(n_clicks):
     return_list = []
-    for _ in range(300):
+    for _ in range(page_capacity):
         try:
             img_path = img_path_list.pop(0)
         except IndexError:
@@ -44,7 +52,21 @@ def popup_100_pics(n_clicks):
         return_list.append(
             html.A(html.Img(src=img_path, style={'max-height': '380px', 'vertical-align': 'middle'}), href=img_path, target='_blank')
         )
-    return return_list
+    remain_count = '还剩{}张'.format(len(img_path_list))
+    return return_list, remain_count
+
+
+@app.callback(
+    dash.dependencies.Output('button_text', 'children'),
+    [
+        dash.dependencies.Input('slider1', 'value')
+    ]
+)
+def set_page_capacity(s_value):
+    global page_capacity
+    page_capacity = s_value
+    button_text = '再来{}张！'.format(page_capacity)
+    return button_text
 
 
 if __name__ == "__main__":
