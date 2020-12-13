51e84a37ba33c6390d35f77e99546acb66afb406
surface@pro.3 <348699103@qq.com>
2020-12-14 00:26:34 +0800

    新增删除空文件夹的功能

--- dashapp/utils.py
+++ dashapp/utils.py
@@ -8,9 +8,11 @@ def image_picker_quantitative(img_base_path, quantity: int, move: bool):
     execution_mode = shutil.move if move else shutil.copy
     img_base_path = os.path.abspath(img_base_path)
     img_list = []
+    empty_folder_maybe = []
     for root_, dirs_, files_ in os.walk(img_base_path):
         for file_ in files_:
             img_list.append(os.path.join(img_base_path, root_, file_))
+        empty_folder_maybe.extend(dirs_)
     img_list.sort()
     n = 0
     for img in tqdm(img_list):
@@ -19,6 +21,9 @@ def image_picker_quantitative(img_base_path, quantity: int, move: bool):
         if not os.path.exists(target_folder_path):
             os.mkdir(target_folder_path)
         execution_mode(img, target_folder_path)
+    if move:
+        for folder_to_del in empty_folder_maybe[::-1]:
+            os.rmdir(folder_to_del)
 
 
 if __name__ == "__main__":
