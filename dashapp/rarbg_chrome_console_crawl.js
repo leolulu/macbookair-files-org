cf6f3cfda6c50b09eafada7e38cd900984c7cbb2
surface@pro.3 <348699103@qq.com>
2021-07-31 02:36:29 +0800

    大幅增加rarbg的自动化程度

--- dashapp/rarbg_chrome_console_crawl.js
+++ dashapp/rarbg_chrome_console_crawl.js
@@ -1,9 +1,136 @@
+var item_count = sessionStorage.getItem('item_count')
+if (item_count == null) {
+    item_count = 0
+}
 for (i of document.getElementsByClassName("lista2")) {
-    console.log(
+    item = [
+        i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].text,
         i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("onmouseover").match("(http.*?jpg)")[0],
         "https://rarbgprx.org" + i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("href")
-    )
+    ]
+    item_count++
+    console.log(item_count)
+    sessionStorage.setItem(item_count, item)
+    sessionStorage.setItem('item_count', item_count)
 }
 var a_list = document.getElementById("pager_links").getElementsByTagName("a")
-setTimeout("a_list[a_list.length-1].click()",5000)
+a_list[a_list.length - 1].click()
 
+
+var item_count = sessionStorage.getItem('item_count')
+for (var i = 0; i <= item_count; i++) {
+    var item = sessionStorage.getItem(i)
+    if (item != null) {
+        console.log(item)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// var data = new Array()
+// var page_count = 1
+// var max = 3
+// var intervalId = null
+
+
+// function crawl() {
+//     console.log("当前页码：" + page_count)
+
+//     if (page_count > max) {
+//         clearInterval(intervalId)
+//         console.log("结束循环...")
+//     }
+
+//     for (i of document.getElementsByClassName("lista2")) {
+//         item = [
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].text,
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("onmouseover").match("(http.*?jpg)")[0],
+//             "https://rarbgprx.org" + i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("href")
+//         ]
+//         data.push(item)
+//     }
+//     console.log(data)
+
+//     page_count++
+
+//     var a_list = document.getElementById("pager_links").getElementsByTagName("a")
+//     a_list[a_list.length - 1].click()
+// }
+
+// intervalId = setInterval(crawl, 5000)
+
+
+
+
+
+
+
+
+
+
+// var page_count = 1
+// var max = 3
+// var intervalId = null
+
+
+// function crawl() {
+//     console.log("当前页码：" + page_count)
+
+//     if (page_count > max) {
+//         clearInterval(intervalId)
+//         console.log("结束循环...")
+//     }
+
+//     for (i of document.getElementsByClassName("lista2")) {
+//         item = [
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].text,
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("onmouseover").match("(http.*?jpg)")[0],
+//             "https://rarbgprx.org" + i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("href")
+//         ]
+//         data.push(item)
+//     }
+//     console.log(data)
+
+//     page_count++
+
+//     var a_list = document.getElementById("pager_links").getElementsByTagName("a")
+//     a_list[a_list.length - 1].click()
+// }
+
+// intervalId = setInterval(crawl, 5000)
+
+
+
+
+
+
+
+
+
+
+// var data = new Array()
+
+// for (var page_count = 1; page_count < 3; page_count++) {
+//     console.log("当前页码：" + page_count)
+//     for (i of document.getElementsByClassName("lista2")) {
+//         item = [
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].text,
+//             i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("onmouseover").match("(http.*?jpg)")[0],
+//             "https://rarbgprx.org" + i.getElementsByClassName("lista")[1].getElementsByTagName("a")[0].getAttribute("href")
+//         ]
+//         data.push(item)
+//     }
+//     var a_list = document.getElementById("pager_links").getElementsByTagName("a")
+//     setTimeout("a_list[a_list.length-1].click()", 5000)
+// }
+
+// console.log(data)
--- dashapp/rarbg_preview.py
+++ dashapp/rarbg_preview.py
@@ -7,23 +7,44 @@ app = dash.Dash(__name__)
 
 pic_height = 150
 
-with open('rarbg_chrome_console_crawl.js', 'r', encoding='utf-8') as f:
+with open('rarbg_data.data', 'r', encoding='utf-8') as f:
     img_url_page_url_list = f.read().split('\n')
-img_url_page_url_list = [i.split(' ')[1:3] for i in list(filter(lambda x:re.match(r'VM\d{3,4}', x), img_url_page_url_list))]
-print(img_url_page_url_list)
+img_url_page_url_list = [re.sub(r"VM\d+:\d+", '', i).replace("\u200b", "").strip().split(",") for i in list(filter(lambda x:re.search(r'http', x), img_url_page_url_list))]
+img_url_page_url_list = [(i[0].split('.')[0], i[1], i[2]) for i in img_url_page_url_list]
+img_url_page_url_list.sort(key=lambda x: x[0])
+for i in img_url_page_url_list[::-1]:
+    with open('rarbg.history', 'r', encoding='utf-8') as f:
+        his = f.read().split('\n')
+    if str(i) not in his:
+        print(i)
+        with open('rarbg.history', 'a', encoding='utf-8') as f:
+            f.write('\n'+str(i))
+    else:
+        img_url_page_url_list.remove(i)
+# print(img_url_page_url_list)
 
 
 container_content = []
+previous_brand = ''
+batch_img_content = []
 for idx, img_url_page_url in enumerate(img_url_page_url_list):
-    container_content.append(
+    brand = img_url_page_url[0]
+    if brand != previous_brand:
+        container_content.extend(batch_img_content)
+        batch_img_content = []
+        container_content.append(html.H1(brand, style={"width": "100%"}))
+    previous_brand = brand
+    batch_img_content.append(
         html.A(html.Img(
-            src=img_url_page_url[0],
-            style={'height': f'{pic_height}px', 'vertical-align': 'middle'},
+            src=img_url_page_url[1],
+            style={'height': f'{pic_height}px', 'verticalAlign': 'middle'},
             id={'type': 'pics', 'index': idx}
-        ), href=img_url_page_url[1], target='_blank'))
+        ), href=img_url_page_url[2], target='_blank'))
+if batch_img_content:
+    container_content.extend(batch_img_content)
 
 app.layout = html.Div([
-    html.Div(container_content, style={'display': 'flex', 'flex-wrap': 'wrap', 'justify-content': 'left'}, id='container'),
+    html.Div(container_content, style={'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'left'}, id='container'),
     html.Div(
         dcc.Slider(min=100, max=1500, step=1, value=pic_height, updatemode='drag', id='slider2'), id='button_container', style={'float': 'right'}
     )
@@ -33,12 +54,12 @@ app.layout = html.Div([
 @app.callback(
     dash.dependencies.Output({'type': 'pics', 'index': dash.dependencies.ALL}, 'style'),
     [dash.dependencies.Input('slider2', 'value')],
-    [dash.dependencies.State('container', 'children')]
+    dash.dependencies.State({'type': 'pics', 'index': dash.dependencies.ALL}, 'children')
 )
 def det_pic_height(s_value, pics):
     global pic_height
     pic_height = s_value
-    return [{'height': f'{pic_height}px', 'vertical-align': 'middle'} for i in range(len(pics))]
+    return [{'height': f'{pic_height}px', 'verticalAlign': 'middle'} for i in range(len(pics))]
 
 
 if __name__ == "__main__":
