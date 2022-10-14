bfad1a9c2fba5d85e43705ad508f13c7fe4b6343
leolulu_sad_python <348699103@qq.com>
2022-10-14 19:53:14 +0800

    新增对于文件名中含有#的情况的支持·改

--- dashapp/image.py
+++ dashapp/image.py
@@ -23,7 +23,7 @@ def get_img_path_list(img_path_list: List):
     temp_img_list = []
     for root, dirs_, files_ in os.walk('./static/img'):
         for file_ in files_:
-            temp_img_list.append(os.path.join(root, file_).replace('\\', '/'))
+            temp_img_list.append(os.path.join(root, file_).replace('\\', '/').replace("#","%23"))
     temp_img_list.sort()
     for temp_img in temp_img_list:
         if temp_img not in img_path_list and temp_img not in browserd_img_list:
