960398874f5523d70909ab5f6e43db4295cee67b
leolulu <348699103@qq.com>
2022-10-17 23:21:10 +0800

    change param

--- loudnorm_audio.py
+++ loudnorm_audio.py
@@ -5,7 +5,7 @@ import subprocess
 
 def cut_video(video_path):
     loudnormed_file_name = '_loudnormed'.join(os.path.splitext(video_path))
-    exe_string = 'ffmpeg -i "{}" -af loudnorm=I=-5:LRA=1:TP=-1.5 -c:v copy "{}"'
+    exe_string = 'ffmpeg -i "{}" -af "loudnorm=I=-16:LRA=11:TP=-1.5, highpass=f=200, lowpass=f=3000" -c:v copy "{}"'
     exe_string = exe_string.format(video_path, loudnormed_file_name)
     print(exe_string)
     subprocess.call(exe_string, shell=True)
