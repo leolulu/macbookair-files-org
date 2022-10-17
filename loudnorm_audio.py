f27f79dab169e2a3db29a78178e9e7fc6edc4882
surface@pro.3 <348699103@qq.com>
2022-10-17 11:50:30 +0800

    新增音量均衡器

---
+++ loudnorm_audio.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import subprocess
+
+
+def cut_video(video_path):
+    loudnormed_file_name = '_loudnormed'.join(os.path.splitext(video_path))
+    exe_string = 'ffmpeg -i "{}" -af loudnorm=I=-5:LRA=1:TP=-1.5 -c:v copy "{}"'
+    exe_string = exe_string.format(video_path, loudnormed_file_name)
+    print(exe_string)
+    subprocess.call(exe_string, shell=True)
+
+
+if __name__ == "__main__":
+    video_path = sys.argv[1]
+    cut_video(os.path.abspath(video_path))
