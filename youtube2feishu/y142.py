9be4d7aae480df326bf0a75c50f4927f9b359983
surface@pro.3 <348699103@qq.com>
2023-03-04 00:41:58 +0800

    fix bug

--- youtube2feishu/y142.py
+++ youtube2feishu/y142.py
@@ -6,7 +6,10 @@ import time
 temp_folder = str(int(time.time()))
 if not os.path.exists(temp_folder):
     os.mkdir(temp_folder)
-log_path = os.path.join(temp_folder, 'log.log')
+log_folder = 'logs'
+if not os.path.exists(log_folder):
+    os.mkdir(log_folder)
+log_path = os.path.join(log_folder, f'{temp_folder}.log')
 
 command = 'python yt_dlp_tool.py  --temp_folder_name "{temp_folder_name}" --dl_dir "{dl_dir}" --postprefix "__l0" {url} > {log_path} 2>&1'.format(
     dl_dir=r"\\192.168.123.222\folder_for_142",
