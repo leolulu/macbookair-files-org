134194b19be6e6dbf7d74ac211546f941e30876b
leolulu <348699103@qq.com>
2023-08-16 15:14:48 +0800

    adjust

--- my_whisperx/my_whisperx.py
+++ my_whisperx/my_whisperx.py
@@ -2,8 +2,8 @@ from faster_whisper import WhisperModel
 
 
 class MyWhisperX:
-    def __init__(self) -> None:
-        self.model_size = WhisperModel("large-v2", device="cuda", compute_type="float16")
+    def __init__(self, model_size="large-v2") -> None:
+        self.model_size = WhisperModel(model_size, device="cuda", compute_type="float16")
 
     def transcribe(self, media_path, beam_size=5, word_timestamps=False):
         segments, info = self.model_size.transcribe(media_path, beam_size=beam_size, word_timestamps=word_timestamps)
