f4b77ea9a99b5dec52979546b661394bf793c2e6
leolulu <348699103@qq.com>
2023-08-16 15:03:51 +0800

    fix bug

--- my_whisperx/my_whisperx.py
+++ my_whisperx/my_whisperx.py
@@ -3,7 +3,7 @@ from faster_whisper import WhisperModel
 
 class MyWhisperX:
     def __init__(self) -> None:
-        self.model_size = WhisperModel("large", device="cuda", compute_type="float16")
+        self.model_size = WhisperModel("large-v2", device="cuda", compute_type="float16")
 
     def transcribe(self, media_path, beam_size=5):
         segments, info = self.model_size.transcribe(media_path, beam_size=beam_size)
