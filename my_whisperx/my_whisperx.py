4519885c7ea07fab79c4c12309c734521a8a2d1b
leolulu <348699103@qq.com>
2023-08-16 15:08:32 +0800

    add function

--- my_whisperx/my_whisperx.py
+++ my_whisperx/my_whisperx.py
@@ -5,7 +5,7 @@ class MyWhisperX:
     def __init__(self) -> None:
         self.model_size = WhisperModel("large-v2", device="cuda", compute_type="float16")
 
-    def transcribe(self, media_path, beam_size=5):
-        segments, info = self.model_size.transcribe(media_path, beam_size=beam_size)
+    def transcribe(self, media_path, beam_size=5, word_timestamps=False):
+        segments, info = self.model_size.transcribe(media_path, beam_size=beam_size, word_timestamps=word_timestamps)
         print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
         return segments, info
