c47c85cca5f29724d2137cb6a24308b791a24216
leolulu <348699103@qq.com>
2023-08-16 14:55:02 +0800

    init

---
+++ my_whisperx/my_whisperx.py
@@ -0,0 +1,11 @@
+from faster_whisper import WhisperModel
+
+
+class MyWhisperX:
+    def __init__(self) -> None:
+        self.model_size = WhisperModel("large", device="cuda", compute_type="float16")
+
+    def transcribe(self, media_path, beam_size=5):
+        segments, info = self.model_size.transcribe(media_path, beam_size=beam_size)
+        print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+        return segments, info
