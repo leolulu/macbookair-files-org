64ad099580491e5fb31eef1a3f9d6a60738820a4
leolulu_sad_python <348699103@qq.com>
2023-09-21 15:33:48 +0800

    fix typo

--- loudnorm_audio.py
+++ loudnorm_audio.py
@@ -3,7 +3,7 @@ import sys
 import subprocess
 
 
-def cut_video(video_path):
+def loudnorm_video(video_path):
     loudnormed_file_name = '_loudnormed'.join(os.path.splitext(video_path))
     exe_string = 'ffmpeg -i "{}" -af "loudnorm=I=-14:LRA=11:TP=-1.5, highpass=f=200, lowpass=f=3000" -c:v copy "{}"'
     exe_string = exe_string.format(video_path, loudnormed_file_name)
@@ -13,4 +13,4 @@ def cut_video(video_path):
 
 if __name__ == "__main__":
     video_path = sys.argv[1]
-    cut_video(os.path.abspath(video_path))
+    loudnorm_video(os.path.abspath(video_path))
