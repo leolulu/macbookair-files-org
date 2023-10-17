f2023068ad8dfa46cbecc45b0cfe721439d66ab8
leolulu <348699103@qq.com>
2023-10-17 22:56:09 +0800

    新增生成缩略图自动计算列数功能

--- thumbnails_maker.py
+++ thumbnails_maker.py
@@ -17,11 +17,15 @@ def get_font_location(frame, content: str, fontFace: int, font_scale: float, thi
     return (start_x, start_y)
 
 
-def generate_thumbnail(video_path, rows, cols):
+def generate_thumbnail(video_path, rows, cols=None):
     # 读取视频
     cap = cv2.VideoCapture(video_path)
     if not cap.isOpened():
         raise UserWarning("无法打开视频文件!")
+
+    if cols is None:
+        height, width, _ = cap.read()[1].shape
+        cols = int(16 * height * rows / 9 / width)
 
     print(f"开始生成视频缩略图，视频路径：{video_path}，行列数：{rows}x{cols}")
 
@@ -107,6 +111,10 @@ if __name__ == "__main__":
         video_path = sys.argv[1]
         rows = 7
         cols = 7
+    if len(sys.argv) == 3:
+        video_path = sys.argv[1]
+        rows = int(sys.argv[2])
+        cols = None
     elif len(sys.argv) == 4:
         video_path = sys.argv[1]
         rows = int(sys.argv[2])
