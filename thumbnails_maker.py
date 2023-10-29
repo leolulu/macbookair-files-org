05b082252f35b1a729ebd2d2c6b2d55e2a21a528
leolulu_sad_python <348699103@qq.com>
2023-10-29 20:20:53 +0800

    修改后缀名，新增处理格式

--- thumbnails_maker.py
+++ thumbnails_maker.py
@@ -100,7 +100,7 @@ def generate_thumbnail(video_path, rows, cols=None):
 
     # 保存缩略图
     output_path_img = os.path.splitext(video_path)[0] + ".jpg"
-    output_path_video = os.path.splitext(video_path)[0] + "_DAthumb.mp4"
+    output_path_video = os.path.splitext(video_path)[0] + ".thb.mp4"
     temp_output_path_img = os.path.join(str(Path.home() / "Downloads"), f"{uuid.uuid4().hex}.jpg")
     temp_output_path_video = os.path.join(str(Path.home() / "Downloads"), f"{uuid.uuid4().hex}.mp4")
     print(f"缩略图保存路径为：{output_path_img}")
@@ -186,7 +186,7 @@ if __name__ == "__main__":
         video_paths = [
             os.path.join(video_path, f)
             for f in os.listdir(video_path)
-            if os.path.splitext(f)[-1].lower() in [".mp4", ".flv", ".avi", ".mpg", ".wmv", ".mpeg", ".mov", ".mkv", ".ts"]
+            if os.path.splitext(f)[-1].lower() in [".mp4", ".flv", ".avi", ".mpg", ".wmv", ".mpeg", ".mov", ".mkv", ".ts", ".rmvb", ".rm"]
         ]
         for video_path in video_paths:
             generate_thumbnail(video_path, rows, cols)
