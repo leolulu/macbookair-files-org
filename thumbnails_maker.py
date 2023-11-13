5200e273d8ccc87666760a9f9f643493b0e002fd
leolulu <348699103@qq.com>
2023-11-13 18:05:52 +0800

    取消自动控制行数

--- thumbnails_maker.py
+++ thumbnails_maker.py
@@ -28,7 +28,7 @@ def generate_thumbnail(video_path, rows, cols=None):
 
     if cols is None:
         height, width, _ = cap.read()[1].shape
-        rows = math.ceil(width / height / (16 / 9) * rows)
+        # rows = math.ceil(((width / height) / (16 / 9 * 2) + 1 / 2) * rows)
         cols = int(16 * height * rows / 9 / width)
 
     print(f"开始生成视频缩略图，视频路径：{video_path}，行列数：{rows}x{cols}")
