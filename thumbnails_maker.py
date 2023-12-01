c8b786f6fda9428526be14c9f18fbc03b39de93d
leolulu <348699103@qq.com>
2023-12-01 13:56:48 +0800

    避免不必要的输出

--- thumbnails_maker.py
+++ thumbnails_maker.py
@@ -171,13 +171,14 @@ def gen_video_thumbnail(video_path, preset, height, fps, duration_in_seconds, fr
             if intermediate_file_dimension is None:
                 intermediate_file_dimension = (intermediate_file_width, intermediate_file_height)  # type: ignore
     # 修复受损的中间文件
-    print(f"开始修复以下受损文件:")
-    print("\n".join(corrupted_file_paths))
-    for corrupted_file_path in corrupted_file_paths:
-        fix_command = 'ffmpeg -f lavfi -i color=c=gray:s={}x{}:d=1 -c:v libx264 -y "{}"'.format(
-            intermediate_file_dimension[0], intermediate_file_dimension[1], corrupted_file_path
-        )
-        subprocess.run(fix_command, shell=True)
+    if corrupted_file_paths:
+        print(f"开始修复以下受损文件:")
+        print("\n".join(corrupted_file_paths))
+        for corrupted_file_path in corrupted_file_paths:
+            fix_command = 'ffmpeg -f lavfi -i color=c=gray:s={}x{}:d=1 -c:v libx264 -y "{}"'.format(
+                intermediate_file_dimension[0], intermediate_file_dimension[1], corrupted_file_path
+            )
+            subprocess.run(fix_command, shell=True)
 
     # 合并中间文件
     command = "ffmpeg "
