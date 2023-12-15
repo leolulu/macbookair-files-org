bbeba9677b28c362caaad83c23fa5858222cea22
leolulu <348699103@qq.com>
2023-12-15 15:04:58 +0800

    增加模糊中文字幕的功能

---
+++ censor_zh_char_in_sub.py
@@ -0,0 +1,17 @@
+import re
+import sys
+import os
+
+
+def censor_zh_char(text):
+    result = re.sub(r"([\u4e00-\u9fa5]+(?:[\s-]+[\u4e00-\u9fa5]+)*)", r'<span class="censored">\1</span>', text)
+    return result
+
+
+if __name__ == "__main__":
+    text_file_path = sys.argv[1]
+    censor_text_file_path = "_censored".join(os.path.splitext(text_file_path))
+    with open(text_file_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    with open(censor_text_file_path, "w", encoding="utf-8") as f:
+        f.write(censor_zh_char(text))
