64d33be3411bb854ce18a1ad83dc099abf7f0130
leolulu_sad_python <348699103@qq.com>
2024-06-01 22:42:43 +0800

    新增抽取mkv里面的字幕的脚本

---
+++ extract_sub.py
@@ -0,0 +1,72 @@
+import json
+import os
+import subprocess
+import sys
+
+
+def get_subtitle_extension(codec_name):
+    # 字幕格式与文件扩展名的映射
+    extension_mapping = {
+        "subrip": "srt",
+        "ass": "ass",
+        "hdmv_pgs_subtitle": "sup",
+        "dvd_subtitle": "sub",
+        "mov_text": "txt",
+    }
+    # 返回相应的扩展名，如果未知则默认返回 'srt'
+    return extension_mapping.get(codec_name, "srt")
+
+
+def extract_subtitles(input_file, language_filter=[]):
+    # 使用 ffprobe 获取文件信息
+    cmd = [
+        "ffprobe",
+        "-loglevel",
+        "error",
+        "-show_entries",
+        "stream=index,codec_name:stream_tags=language",
+        "-select_streams",
+        "s",
+        "-of",
+        "json",
+        input_file,
+    ]
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    streams = json.loads(result.stdout)
+
+    # 遍历所有字幕流
+    for stream in streams["streams"]:
+        index = stream["index"]
+        codec_name = stream["codec_name"]
+        language = stream.get("tags", {}).get("language", "unk")
+        if language_filter and all(not acceptable_language_fragment in language for acceptable_language_fragment in language_filter):
+            continue
+
+        # 获取正确的文件扩展名
+        extension = get_subtitle_extension(codec_name)
+
+        # 设置输出文件名
+        output_file = f"{input_file.rsplit('.', 1)[0]}_{language}.{extension}"
+
+        # 使用 ffmpeg 提取字幕
+        ffmpeg_cmd = ["ffmpeg", "-loglevel", "error", "-i", input_file, "-map", f"0:{index}", "-c", "copy", output_file]
+        subprocess.run(ffmpeg_cmd)
+        print(f"Extracted subtitles to {output_file}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py input_file.mkv")
+        sys.exit(1)
+
+    input_path = os.path.abspath(sys.argv[1])
+    language_filter = []
+    if len(sys.argv) > 2:
+        language_filter = sys.argv[2].split(",")
+
+    if os.path.isdir(input_path):
+        for file_in_folder in os.listdir(input_path):
+            if os.path.splitext(file_in_folder)[-1].lower() == ".mkv":
+                extract_subtitles(file_in_folder, language_filter)
+    else:
+        extract_subtitles(input_path, language_filter)
